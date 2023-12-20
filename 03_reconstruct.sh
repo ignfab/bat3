d946510b78f27925926f6fc79b0c600f73b6cce5
Emmanuel S <5435148+esgn@users.noreply.github.com>
2023-12-20 17:41:08 +0100

    fix typo

--- 03_reconstruct.sh
+++ 03_reconstruct.sh
@@ -2,5 +2,5 @@
 
 rm -rf geoflow_results
 mkdir geoflow_results
-python3 scripts/generate_geoflow_command.py
+python3 scripts/generate_geoflow_commands.py
 time parallel --joblog parallel_geoflow.log -j $(nproc) < geoflow_commands.txt
